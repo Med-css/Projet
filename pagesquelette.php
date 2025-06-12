1705efc77823b1df46241461ff488b723e2a95dc
Med.css <erraesmehdi@hotmail.com>
2025-06-12 17:36:37 +0200

    squelette de page

---
+++ pagesquelette.php
@@ -0,0 +1,12 @@
+<?php require 'Header.php'; ?>
+<main>
+    <div class="titlePage">
+        <h1>Titre de la page</h1>
+    </div>
+    <div class="contentPage"></div>
+
+
+<!-- </main> se ferme dans le footer pas besoin de le fermer ici -->
+
+
+<?php require 'Footer.php'; ?>
