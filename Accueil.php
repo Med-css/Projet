15e814b25d0379f1a621756f914e167bd86913fa
Med.css <erraesmehdi@hotmail.com>
2025-06-14 14:37:53 +0200

    animation

--- Accueil.php
+++ Accueil.php
@@ -1,11 +1,44 @@
 <main>
     <div class="accueil">
-    <div class="title">
-        <h1>Titre lorem lorem</h1>
+        <div class="title">
+            <h1>Titre lorem lorem</h1>
+        </div>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aperiam consequatur aliquid explicabo dolores provi</p>
+        </div>
+        <img src="./img/imgaccueil.png" alt="" class="imgaccueil">
     </div>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aperiam consequatur aliquid explicabo dolores provi</p>
-    </div>
-    <img src="./img/imgaccueil.png" alt="" class="imgaccueil">
-</div>
     <div class="accueilbg"></div>
+
+    <script>
+        gsap.from(".accueilbg", {
+            duration: 1.5, // Durée de l'animation en secondes
+            scaleY: 0, // Commence avec une échelle verticale de 0
+            transformOrigin: "bottom", // L'animation part du bas
+            ease: "power3.out" // Type d'accélération
+        });
+
+        gsap.from(".title h1", {
+            duration: 1,
+            y: -50, // Déplace le titre de 50px vers le haut
+            opacity: 0, // Commence avec une opacité de 0
+            ease: "power2.out",
+            delay: 0.5 // Délai avant le début de l'animation
+        });
+
+        gsap.from(".content p", {
+            duration: 1,
+            y: 50, // Déplace le paragraphe de 50px vers le bas
+            opacity: 0,
+            ease: "power2.out",
+            delay: 0.8 // Délai après l'animation du titre
+        });
+
+        gsap.from(".imgaccueil", {
+            duration: 1.2,
+            x: 100, // Déplace l'image de 100px vers la droite
+            opacity: 0,
+            ease: "power2.out",
+            delay: 1 // Délai après l'animation du contenu
+        });
+    </script>
--- Header.php
+++ Header.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/global.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
 
 </head>
 <body>
@@ -81,5 +83,14 @@
                     textElement.setAttribute('data-length', 'very-long');
                 }
             });
+        });
+    </script>
+    <script>
+        const headerCards = document.querySelectorAll('.headerCard');
+        headerCards.forEach((card, index) => {
+               // ajouter un delai animamtion à chacune avec 0.2s d'écrat à chaque petit à petit incrémentation
+            // exemple la premier animation delay 0.2 la deuxième 0.4 la troisième 0.6 etc...
+      
+            card.style.animationDelay = `${index * 0.1}s`;
         });
     </script>
